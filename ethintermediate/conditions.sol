a0574e4b70559735de81d6add7a14613d17c4416
Edwinturn <145699411+Edwinturn@users.noreply.github.com>
2023-10-02 00:16:28 +0000

    Update conditions.sol

--- ethintermediate/conditions.sol
+++ ethintermediate/conditions.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
+contract conditions {
+   uint public x;
+   uint public y;
+   
+   function saveX(uint newX) public{ 
+    require(newX < 5, "x should always be lesser than 5");
+    assert(newX != 0);
+   x = newX;
+   }
+
+   function addToY() public {
+    if (y > 10) {
+        revert("y is already greater than 10");
+    }
+   y +=x;
+   }
+
+   
+}
